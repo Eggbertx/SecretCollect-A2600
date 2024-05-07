104fc778ff364ac57684ce01b26fd7380cc4923c
Eggbertx <eggbertx@users.noreply.github.com>
2024-05-07 12:46:57 -0700

    Add sprite, joystick movement

---
+++ consts.h
@@ -0,0 +1,12 @@
+BGColor = #$00
+WallColor = #$C6
+TextColor = #$06
+SBColor = #$41
+SecretColor = #$1A
+SpeedItemColor = #$83
+SpriteHeight = 8
+
+JOY_UP = %00100000
+JOY_DOWN = %00010000
+JOY_LEFT = %01000000
+JOY_RIGHT = %10000000
